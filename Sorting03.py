abfb4ce6059bc415522462a869819d5ad4faeaa6
Strawman Dave <davidgb.strawman@gmail.com>
2026-09-10 07:37:06 -0600

    Add files via upload

---
+++ Sorting03.py
@@ -0,0 +1,18 @@
+def main():
+    print("Hello World")
+    #put sorting alg into a list
+
+    #run each alg thorugh measuring wokr
+    #work is a dat a compaer or dat swap/move
+    #size of the char is on the outside when making table
+    
+    #do it again with mostly sorted data
+    #create a mostly sorted function
+
+    #make the oupt of these be tables
+
+    #then tkae dat into a sheet software that can create a line chart
+
+    #makea log log chare meaing a char that scales by 2^N
+
+main()
